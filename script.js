932d91bfd5a0f325092d6b7d5adec0143c721f50
yogeshwaz <yoogeshwazz@gmail.com>
2025-07-04 12:46:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function submit(){
+    debugger
+var fname=document.getElementById('fname')
+var lname=document.getElementById('lname')
+var fullname=fname.value+" "+lname.value
+console.log(fullname)
+}
