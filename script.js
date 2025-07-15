fc8a4394a4575d4623e98bb56df46358f9ee2b7d
yogeshwaz <yoogeshwazz@gmail.com>
2025-07-15 13:53:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-function submit(){
-    debugger
-var fname=document.getElementById('fname')
-var lname=document.getElementById('lname')
-var fullname=fname.value+" "+lname.value
-console.log(fullname)
-}
+function submit() {
+    debugger;
+    var fullname = document.getElementById('fullname');
+    console.log(fullname.value);
+
+    var phno = document.getElementById('phno');
+    console.log(phno.value);
+
+    var email = document.getElementById('email');
+    console.log(email.value);
+
+    // Get selected food preference
+    var selectedFood = "";
+    var selectedfoodRadio = document.querySelector('input[name="food"]:checked');
+    if (selectedfoodRadio) {
+        selectedFood = selectedfoodRadio.value;
+    }
+
+    console.log("Selected Food Preference:", selectedFood);
+    document.getElementById('selectedValue')?.remove();
+
+    var selectedtshirt = "";
+    var selectedtshirtRadio = document.querySelector('input[name="tshirt"]:checked');
+    if (selectedtshirtRadio) {
+        selectedtshirt = selectedtshirtRadio.value;
+    }
+
+    console.log("Selected tshirt :", selectedtshirt);
+    document.getElementById('selectedValue')?.remove(); 
+
+
+    var selectedgender = "";
+    var selectedgenderRadio = document.querySelector('input[name="gender"]:checked');
+    if (selectedgenderRadio) {
+        selectedgender = selectedgenderRadio.value;
+    }
+
+    console.log("Selected gender :", selectedgender);
+    document.getElementById('selectedValue')?.remove(); 
+
+
+
+  }
---
+++ style.cs
@@ -0,0 +1,27 @@
+h2{
+    text-decoration: underline;
+    text-align: center;
+    color:black;
+}
+#fullname{
+    font-size: large;
+}
+#phno{
+    font-size: large;
+}
+#email{
+    font-size: large;
+}
+#button{
+    
+    font-size: x-large;
+    background-color: blue;
+    
+    
+}
+body{
+    font-size: xx-large;
+    background-image:url("groundimage.avif");
+    background-size: cover;
+}
+
