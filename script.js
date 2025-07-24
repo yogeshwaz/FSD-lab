db2c923cf4068870234c926061802845658d0007
yogeshwaz <yoogeshwazz@gmail.com>
2025-07-24 13:44:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
 function submit() {
-    debugger;
     var fullname = document.getElementById('fullname');
     console.log(fullname.value);
 
     var phno = document.getElementById('phno');
+    var errorMsg = document.getElementById('errorMsg');
+
+    var phnoRegex = /^\d+$/;
+    if (!phnoRegex.test(phno.value)) {
+        errorMsg.textContent = "Please enter a valid phone number containing only digits.";
+        return;
+    }
     console.log(phno.value);
 
     var email = document.getElementById('email');
     console.log(email.value);
-
-    // Get selected food preference
-    var selectedFood = "";
-    var selectedfoodRadio = document.querySelector('input[name="food"]:checked');
-    if (selectedfoodRadio) {
-        selectedFood = selectedfoodRadio.value;
-    }
-
-    console.log("Selected Food Preference:", selectedFood);
-    document.getElementById('selectedValue')?.remove();
-
-    var selectedtshirt = "";
-    var selectedtshirtRadio = document.querySelector('input[name="tshirt"]:checked');
-    if (selectedtshirtRadio) {
-        selectedtshirt = selectedtshirtRadio.value;
-    }
-
-    console.log("Selected tshirt :", selectedtshirt);
-    document.getElementById('selectedValue')?.remove(); 
-
-
-    var selectedgender = "";
-    var selectedgenderRadio = document.querySelector('input[name="gender"]:checked');
-    if (selectedgenderRadio) {
-        selectedgender = selectedgenderRadio.value;
-    }
-
-    console.log("Selected gender :", selectedgender);
-    document.getElementById('selectedValue')?.remove(); 
-
-
-
-  }
+    
+    var feedback = document.getElementById('Feedback');
+    console.log(feedback.value);
+}
